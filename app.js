dc18a511c963b9ae0b3e1399a10d7ad3970b1d9d
MicahWeiss <micahjweiss@gmail.com>
2018-10-01 12:35:18 -0500

    working on class implementation

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// TODO
+var App = () => (
+    <div>
+      <h2>My Grocery List</h2>
+      <GroceryList items = {['Kale', 'Bees']} />
+    </div>
+  );
+
+var GroceryList = (props) => (
+    <ul>
+        {props.items.map(name => <GroceryListItem name = {item} /> )}
+    </ul>
+);
+var GroceryListItem = (props) => (<li>{props.name}</li>); 
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
